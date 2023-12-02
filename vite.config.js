6c230368783abefa0eebc45b0626039c6b47928d
IrinaNezlobina <85892976+IrinaNezlobina@users.noreply.github.com>
2023-12-02 22:16:55 +0400

    Initial dist subtree commit

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/kinopoisk-explorer/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
